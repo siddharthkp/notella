9f60fd3b6c8035e12273c8c7abad11c1bd1e8b97
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-12-25 03:31:17 +0530

    Night theme!

--- interface.js
+++ interface.js
@@ -1,6 +1,8 @@
 const textarea = document.getElementsByTagName('textarea')[0]
 const sidebar = document.getElementsByTagName('sidebar')[0]
+const list = document.getElementsByTagName('list')[0]
 const toggle = document.getElementsByTagName('toggle')[0]
+const night = document.getElementsByTagName('night')[0]
 
 const emptyNotes = {
     '_new note': ''
@@ -14,7 +16,7 @@ const renderSidebar = (notes) => {
     let titles = Object.keys(notes).sort()
     let html = ''
     for (let title of titles) html += `<div onclick="changeNote(event)">${title}</div>`
-    sidebar.innerHTML = html
+    list.innerHTML = html
 }
 
 const changeNote = (event) => {
@@ -70,13 +72,6 @@ const debounce = (func) => {
 	}
 }
 
-/*
-    Get local copy first
-    Get's overwrite with remote copy (store.js)= source of truth
-*/
-let notes = Object.assign({}, emptyNotes, JSON.parse(localStorage.getItem('notes')))
-renderSidebar(notes)
-
 const reset = () => {
     notes = emptyNotes
     saveNotes()
@@ -91,19 +86,57 @@ const toggleSidebar = () => {
     if (textareaVisible) {
         toggle.innerHTML = '☰☰'
         toggle.style.float = 'left'
-        toggle.style.top = -20;
-        toggle.style.left = -7;
+        toggle.style.top = -20
+        toggle.style.left = -7
         textarea.focus()
     } else {
         toggle.innerHTML = '✕'
-        toggle.style.top = -7;
-        toggle.style.left = -20;
+        toggle.style.top = -7
+        toggle.style.left = -20
         toggle.style.float = 'right'
     }
 }
 
+const setTheme = () => {
+    let theme, icon
+    if (localStorage.night) {
+        theme = {
+            backgroundColor: '#20222B',
+            color: '#EFEFEF'
+        }
+        icon = '☀️'
+    } else {
+        theme = {
+            backgroundColor: '#EFEFEF',
+            color: '#333'
+        }
+        icon = '🌚'
+    }
+
+    Object.assign(document.body.style, theme)
+    Object.assign(textarea.style, theme)
+    night.innerHTML = icon
+}
+
+const toggleTheme = () => {
+    if (localStorage.night) delete localStorage.night
+    else localStorage.night = true
+    setTheme()
+}
+
 textarea.addEventListener('keyup', debounce(saveNote))
 toggle.addEventListener('click', toggleSidebar)
+night.addEventListener('click', toggleTheme)
+
+/*
+    Get local copy first
+    Get's overwrite with remote copy (store.js)= source of truth
+*/
+let notes = Object.assign({}, emptyNotes, JSON.parse(localStorage.getItem('notes')))
+renderSidebar(notes)
+setTheme()
+
+/* Register service worker */
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js', { scope: '/' })
